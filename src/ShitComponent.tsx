4be4a2c4bc6045ba8a05407b253ef4a8b6d228e4
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-16 11:13:33 +0930

    save

---
+++ src/ShitComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SharedComponent from './SharedComponent';
+import moment from 'moment';
+import _ from 'lodash';
+const MyComponent = () => {
+  return (
+    <div>
+      {_.get([1, 2, 3], 0)}
+      {moment().format()}
+      <SharedComponent></SharedComponent>
+    </div>
+  );
+};
+
+export default MyComponent;
