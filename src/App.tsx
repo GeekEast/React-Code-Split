b51b7f3dcc12390e45154c9a0ddc133b416e1acb
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-19 23:36:58 +0930

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { MyComponent, ShitComponent } from './components';
 function App() {
   return (
     <div className="App">
-      <MyComponent />
-      <ShitComponent />
+      App
     </div>
   );
 }
