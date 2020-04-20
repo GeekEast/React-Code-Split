6b966e3513f1fe52f9f9bc111c70a3ef68430e14
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-20 16:08:39 +0930

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React, { Fragment } from 'react';
 import { Router, Link } from '@reach/router';
-import Time from './Time';
-import Calendar from './Calendar';
+
 
 const App = () => {
   return (
     <Fragment>
-      <div>
-        <Link to="time">Time</Link>
-      </div>
-      <div>
-        <Link to="calendar">Calendar</Link>
-      </div>
-      <Router>
-        <Time path="time" />
-        <Calendar path="calendar" />
-      </Router>
+      <div>App</div>
+
     </Fragment>
   );
 };
